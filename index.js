52825b52743ea8511b959359ceba50cb8b2270aa
Marten <martenzander@gmail.com>
2017-08-01 01:19:06 +0200

    added class generation based on config

--- index.js
+++ index.js
@@ -11,24 +11,26 @@ module.exports = postcss.plugin( 'postcss-mesh', function ( options ) {
 			gutter: '30px',
 			display: 'inline-block',
 			columns: '12',
-			width: 'fluid',
+			containerWidth: 'fluid',
+			mobileFirst : true,
+
 			viewports:{
 				xlg: {
-					width: '1440px',
+					containerWidth: '1440px',
 					viewport: '1560px',
 				},
 				lg: {
-					width: '1140px',
+					containerWidth: '1140px',
 					viewport: '1200px',
 				},
 				md: {
-					width: '930px',
+					containerWidth: '930px',
 					viewport: '1024px',
 				},
 				sm: {
-					width: '510px',
+					containerWidth: '510px',
 					viewport: '768px',
-				}
+				},
 			},
 		};
 
@@ -40,9 +42,16 @@ module.exports = postcss.plugin( 'postcss-mesh', function ( options ) {
 
 		// final grid settings
 		var mergedSettings = {};
+		mergedSettings.sortedViewports = {};
+
+		// viewport order reference
+		var tempSortedViewports = [];
 
 		// generated CSS
 		var mesh = postcss.root();
+
+		// mobile first or desktop first
+		var queryCondition = '';
 
 		/*=====  End of constants and variables  ======*/
 
@@ -77,6 +86,7 @@ module.exports = postcss.plugin( 'postcss-mesh', function ( options ) {
 		function mergeFinalSettings(){
 			inlineSettings = getInlineSettings();
 
+			// merge settings
 			for( var key in defaultSettings){
 				var curDefaultProperty = defaultSettings[key];
 				var curConfigProperty = configSettings[key];
@@ -86,46 +96,198 @@ module.exports = postcss.plugin( 'postcss-mesh', function ( options ) {
 				if( curConfigProperty !== undefined ) mergedSettings[key] = curConfigProperty;
 				if( curInlineProperty !== undefined ) mergedSettings[key] = curInlineProperty;
 			}
+
+			// sort viewports
+			for (var key in mergedSettings.viewports){
+				var currentViewport = mergedSettings.viewports[key];
+				var currentBreakpoint = currentViewport.viewport;
+					 currentBreakpoint = parseInt(currentBreakpoint.substring(0, currentBreakpoint.length - 2));
+				tempSortedViewports.push(currentBreakpoint);
+			}
+
+			tempSortedViewports = mergedSettings.mobileFirst === 'true' ? tempSortedViewports.sort(function(a, b){return a - b}) : tempSortedViewports.sort(function(a, b){return b - a});
+
+			for (var i = 0; i < tempSortedViewports.length; i++){
+				var breakpoint = tempSortedViewports[i];
+
+				var relatedViewport = {};
+
+				for(var key in mergedSettings.viewports){
+					var currentSetting = mergedSettings.viewports[key];
+					var currentViewport = currentSetting.viewport;
+					var currentBreakpoint = parseInt(currentViewport.substring(0,currentViewport.length - 2));
+
+					if(currentBreakpoint === breakpoint) {
+						relatedViewport = currentSetting
+						relatedViewport.name = key;
+
+						mergedSettings.sortedViewports[key] = relatedViewport;
+					};
+				}
+			}
+
+			// set queryCondition based on mobile first property
+			queryCondition = mergedSettings.mobileFirst === 'true' ? 'min-width' : 'max-width';
 		}
 
 		function getMediaQuery(viewport, width){
 			var atRule = postcss.atRule();
 			var rule = postcss.rule();
 			rule.selector = '.mesh-container';
-			atRule.name = `media (min-width : ${viewport})`;
+			atRule.name = `media (${queryCondition} : ${viewport})`;
 			rule.append(postcss.decl({prop : 'max-width' , value : width }));
 			atRule.append(rule);
 			return atRule;
 		}
 
-		function generateCSS(){
-			var meshContainer = postcss.root();
-			var meshBaseStyles = postcss.rule();
-			meshBaseStyles.selector = '.mesh-container';
+		// generate rules for .mesh-container
+		function getMeshContainerRules(){
+			var rules = [];
+			var meshContainerRules = postcss.rule();
+			meshContainerRules.selector = '.mesh-container';
 
 			// set display
-			meshBaseStyles.append(postcss.decl({prop:'display', value: 'block'}));
+			meshContainerRules.append(postcss.decl({prop:'display', value: 'block'}));
 			// set margin
-			meshBaseStyles.append(postcss.decl({prop:'margin', value: '0 auto'}));
+			meshContainerRules.append(postcss.decl({prop:'margin', value: '0 auto'}));
 			// set max-width
-			mergedSettings['width'] == 'fluid' ? meshBaseStyles.append(postcss.decl({prop:'max-width', value: '100%'})) : meshBaseStyles.append(postcss.decl({prop:'max-width', value: mergedSettings['width']}));
+			mergedSettings['containerWidth'] == 'fluid' ? meshContainerRules.append(postcss.decl({prop:'max-width', value: '100%'})) : meshContainerRules.append(postcss.decl({prop:'max-width', value: mergedSettings['width']}));
 			// set padding
 			var gutterSize = parseInt(mergedSettings['gutter'].substring(0,mergedSettings['gutter'].length - 1));
-			meshBaseStyles.append(postcss.decl({prop:'padding',value: `${gutterSize/2}px` }));
+			meshContainerRules.append(postcss.decl({prop:'padding',value: `0 ${gutterSize/2}px` }));
 			// set position
-			meshBaseStyles.append(postcss.decl({prop:'position',value:'relative'}));
+			meshContainerRules.append(postcss.decl({prop:'position',value:'relative'}));
 			// set width
-			meshBaseStyles.append(postcss.decl({prop:'width', value: '100%'}));
+			meshContainerRules.append(postcss.decl({prop:'width', value: '100%'}));
 
-			// append .mesh-container base styles
-			meshContainer.append(meshBaseStyles);
+			rules.push(meshContainerRules);
 
-			for(var key in mergedSettings.viewports){
-				meshContainer.append(getMediaQuery(mergedSettings.viewports[key].viewport,mergedSettings.viewports[key].width));
+			for (var key in mergedSettings.sortedViewports){
+				var currentViewport = mergedSettings.sortedViewports[key];
+				var breakpoint = currentViewport.viewport;
+				var maxWidth = currentViewport.containerWidth;
+				var atRule = postcss.atRule();
+				atRule.name = `media (${queryCondition} : ${breakpoint})`;
+				// update properties
+				if("gutter" in currentViewport) gutterSize = parseInt(currentViewport.gutter.substring(0,currentViewport.gutter.length - 1)) || gutter;
+
+				// common viewport rules
+				var meshContainerRule = postcss.rule();
+					 meshContainerRule.selector = `.mesh-container`;
+
+				// set padding
+				meshContainerRule.append(postcss.decl({prop:'padding', value: `0 ${gutterSize/2}px`}));
+				// set max-width
+				meshContainerRule.append(postcss.decl({prop : 'max-width' , value : maxWidth }));
+
+				atRule.append(meshContainerRule);
+
+				rules.push(atRule);
 			}
 
+			return rules;
+		}
 
-			mesh.append(meshContainer);
+		// generate rules for .mesh-collapse
+		function getMeshCollapseRules(){
+			var rules = [];
+			var meshCollapseRule = postcss.rule();
+			meshCollapseRule.selector = '.mesh-collapse';
+
+			// set display
+			meshCollapseRule.append(postcss.decl({prop:'display', value: 'block'}));
+			// set margin
+			var gutterSize = parseInt(mergedSettings['gutter'].substring(0,mergedSettings['gutter'].length - 1));
+			meshCollapseRule.append(postcss.decl({prop:'margin', value: `0 -${gutterSize/2}px`}));
+
+			rules.push(meshCollapseRule);
+
+			for (var key in mergedSettings.sortedViewports){
+				var currentViewport = mergedSettings.sortedViewports[key];
+				var breakpoint = currentViewport.viewport;
+				var atRule = postcss.atRule();
+				atRule.name = `media (${queryCondition} : ${breakpoint})`;
+				// update properties
+				if("gutter" in currentViewport) gutterSize = parseInt(currentViewport.gutter.substring(0,currentViewport.gutter.length - 1)) || gutter;
+
+				// common viewport rules
+				var meshCollapseRule = postcss.rule();
+					 meshCollapseRule.selector = `.mesh-collapse`;
+
+				// set margin
+				meshCollapseRule.append(postcss.decl({prop:'margin', value: `0 -${gutterSize/2}px`}));
+
+				atRule.append(meshCollapseRule);
+
+				rules.push(atRule);
+			}
+
+			return rules;
+		}
+
+		// generate rules for .mesh-column
+		function getMeshColumnRules(){
+			var rules = [];
+			var meshColumnRule = postcss.rule();
+				 meshColumnRule.selector = `[class*="mesh-column"]`;
+			var displayType = mergedSettings.display;
+			var columns = mergedSettings.columns;
+			var gutterSize = parseInt(mergedSettings['gutter'].substring(0,mergedSettings['gutter'].length - 1));
+
+			// set display
+			meshColumnRule.append(postcss.decl({prop:'display', value: `${displayType}`}));
+			// set padding
+			meshColumnRule.append(postcss.decl({prop:'padding', value: `0 ${gutterSize/2}px`}));
+
+			rules.push(meshColumnRule);
+
+			for (var key in mergedSettings.sortedViewports){
+				var currentViewport = mergedSettings.sortedViewports[key];
+				var viewportName = currentViewport.name;
+				var breakpoint = currentViewport.viewport;
+				var atRule = postcss.atRule();
+				atRule.name = `media (${queryCondition} : ${breakpoint})`;
+				// update properties
+				columns = currentViewport.columns || columns;
+				if("gutter" in currentViewport) gutterSize = parseInt(currentViewport.gutter.substring(0,currentViewport.gutter.length - 1)) || gutter;
+
+				// common viewport rules
+				var meshColumnRule = postcss.rule();
+					 meshColumnRule.selector = `[class*="mesh-column-${viewportName}"]`;
+
+				// set padding
+				meshColumnRule.append(postcss.decl({prop:'padding', value: `0 ${gutterSize/2}px`}));
+
+				atRule.append(meshColumnRule);
+
+				var columnSingleWidth = 100/columns;
+
+				for(var i = 0; i < columns; i++){
+
+					var meshColumnRule = postcss.rule();
+					meshColumnRule.selector = `.mesh-column-${viewportName}-${i+1}`;
+					// set width
+					meshColumnRule.append(postcss.decl({prop:'width',value: `${columnSingleWidth * (i+1)}%`}));
+
+					atRule.append(meshColumnRule);
+				}
+
+				rules.push(atRule);
+			}
+
+			return rules;
+		}
+
+		// generate styles for base classes
+		function generateCSS(){
+
+			// append .mesh-container base styles
+			mesh.append(getMeshContainerRules());
+			// append .mesh-collapse base styles
+			mesh.append(getMeshCollapseRules());
+			// append .mesh-column base styles
+			mesh.append(getMeshColumnRules());
+
 			input.append(mesh);
 		}
 
